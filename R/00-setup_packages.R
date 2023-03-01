e77aadffd1b2f9156b15996c926ad16a81ff48d2
Adrien Le Guillou <git@aleguillou.org>
2023-03-01 08:45:03 +0100

    Initial commit

---
+++ R/00-setup_packages.R
@@ -0,0 +1,17 @@
+# This code installs the packages only available on GitHub (not on CRAN)
+renv::install(c(
+  "EpiModel/ARTnet",
+  "EpiModel/EpiModelHIV-p@main",
+  "EpiModel/EpiModelHPC",
+  "EpiModel/slurmworkflow"
+))
+
+# This code finds and install the libraries used by the project (CRAN version)
+renv::hydrate()
+
+# Force `renv` to discover the following packages
+if (FALSE) {
+  library("rmarkdown")
+  library("pkgload")
+  library("sessioninfo")
+}
